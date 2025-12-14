b0f7b936658f1356fcb9683dea347c2960d2ca50
Slava Mihaelov <slava.m@pandats.com>
2025-12-14 22:48:47 +0200

    Added tile struct and constructor

--- internal/domain/game/tile.go
+++ internal/domain/game/tile.go
@@ -18,8 +18,8 @@ type Tile struct {
 
 func NewTile(color Color, value uint8) Tile {
 	return Tile{
-		ID:    "R1-1",
-		Value: 1,
-		Color: ColorPurple,
+		ID:    string(color) + "-" + string(value),
+		Value: value,
+		Color: color,
 	}
 }

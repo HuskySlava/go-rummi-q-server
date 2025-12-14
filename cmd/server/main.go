73cc638aeb681f1b736e5ccfcf49009884375052
Slava Mihaelov <slava.m@pandats.com>
2025-12-14 23:37:08 +0200

    Config init

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
-	"go-rummi-q-server/internal/domain/game"
+	"go-rummi-q-server/internal/config"
+	"log"
 )
 
 func main() {
-	game.Test()
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatal("Failed loading config", err)
+	}
+	log.Println("CFG:", cfg)
 }
---
+++ internal/config/config.go
@@ -0,0 +1,45 @@
+package config
+
+import (
+	"gopkg.in/yaml.v3"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+type Config struct {
+	HttpTimeoutSeconds int `yaml:"httpTimeoutSeconds"`
+}
+
+func Load() (*Config, error) {
+	exePath, err := os.Executable()
+	if err != nil {
+		log.Println("Error getting executable path:", err)
+		return nil, err
+	}
+
+	exeDir := filepath.Dir(exePath)
+	path := filepath.Join(exeDir, "config.yaml")
+
+	// Try to load config.yaml relative to the binary
+	res, err := os.ReadFile(path)
+	if err != nil {
+		log.Println("Error reading config from binary dir:", err)
+		log.Println("Trying to read the config from current directory (pwd)")
+
+		// Fallback to read config from pwd (for dev with go run ...)
+		res, err = os.ReadFile("config.yaml")
+		if err != nil {
+			log.Println("Error reading config from pwd:", err)
+			return nil, err
+		}
+	}
+
+	var cfg *Config
+	if err := yaml.Unmarshal(res, &cfg); err != nil {
+		log.Println("Error reading YAML:", err)
+		return nil, err
+	}
+
+	return cfg, nil
+}

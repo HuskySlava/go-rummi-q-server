85058684e7a3a07321a095f5d70d6359056b4425
Slava Mihaelov <slava.m@pandats.com>
2025-12-15 21:54:36 +0200

    Start http server from main

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"go-rummi-q-server/internal/config"
+	transport "go-rummi-q-server/internal/transport/http"
 	"log"
 )
 
 func main() {
 	cfg, err := config.Load()
 	if err != nil {
-		log.Fatal("Failed loading config", err)
+		log.Fatalf("Failed loading config:, %v", err)
 	}
 	log.Println("CFG:", cfg)
+
+	transport.StartServer()
 }

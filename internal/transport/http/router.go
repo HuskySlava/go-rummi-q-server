67dd9bd90dd930b8aa3b660ca37db798768909e7
Slava Mihaelov <slava.m@pandats.com>
2025-12-15 21:53:14 +0200

    Basic router.go for the http server with a test

---
+++ internal/transport/http/router.go
@@ -0,0 +1,17 @@
+package transport
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func NewRouter() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		status, err := fmt.Fprint(w, "Welcome to the Home Page")
+		fmt.Println("Status:", status, "Error", err)
+	})
+
+	return mux
+}

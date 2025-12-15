e07110ba72016e882aa2997c74f83982a155e8a5
Slava Mihaelov <slava.m@pandats.com>
2025-12-15 22:46:15 +0200

    Adjusted router test message

--- internal/transport/http/router.go
+++ internal/transport/http/router.go
@@ -9,7 +9,7 @@ func NewRouter() *http.ServeMux {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		status, err := fmt.Fprint(w, "Welcome to the Home Page")
+		status, err := fmt.Fprint(w, "Welcome to Rummi-Q-Server")
 		fmt.Println("Status:", status, "Error", err)
 	})
 

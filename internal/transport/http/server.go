cb6abd08566295e00c8ec7c89b4ac616b6b22109
Slava Mihaelov <slava.m@pandats.com>
2025-12-17 21:59:39 +0200

    Server security headers

--- internal/transport/http/server.go
+++ internal/transport/http/server.go
@@ -8,14 +8,28 @@ import (
 	"time"
 )
 
+func securityHeaders(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.Header().Set("X-Frame-Options", "DENY")
+		w.Header().Set("X-XSS-Protection", "1; mode=block")
+		w.Header().Set("Referrer-Policy", "no-referrer")
+		w.Header().Set("Content-Security-Policy", "default-src 'self'")
+		w.Header().Set("Strict-Transport-Security", "max-age=63072000; includeSubDomains")
+		next.ServeHTTP(w, r)
+	})
+}
+
 func StartServer(httpConfig config.HTTPConfig) error {
 	mux := NewRouter()
+
+	securedMux := securityHeaders(mux)
 
 	addr := httpConfig.ListenHost + ":" + strconv.Itoa(httpConfig.ListenPort)
 
 	srv := &http.Server{
 		Addr:         addr,
-		Handler:      mux,
+		Handler:      securedMux,
 		ReadTimeout:  time.Duration(httpConfig.Timeout) * time.Second,
 		WriteTimeout: time.Duration(httpConfig.Timeout) * time.Second,
 	}

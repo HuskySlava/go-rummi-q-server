2beb06789749d905e30fde4f9033b92fd474e1a0
Slava Mihaelov <slava.m@pandats.com>
2025-12-20 22:18:02 +0200

    new game route

---
+++ internal/domain/lobby/lobby.go
@@ -0,0 +1,25 @@
+package lobby
+
+import (
+	"github.com/google/uuid"
+	"go-rummi-q-server/internal/domain/game"
+	"time"
+)
+
+type Lobby struct {
+	ID         uuid.UUID
+	StartTime  time.Time
+	LastActive time.Time
+	IsGame     bool
+	Players    *[]game.Player
+	Game       *game.Game
+}
+
+func NewLobby() *Lobby {
+	return &Lobby{
+		ID:         uuid.New(),
+		StartTime:  time.Now(),
+		LastActive: time.Now(),
+		IsGame:     false,
+	}
+}
--- internal/transport/http/router.go
+++ internal/transport/http/router.go
@@ -1,7 +1,9 @@
 package transport
 
 import (
+	"encoding/json"
 	"fmt"
+	"go-rummi-q-server/internal/domain/lobby"
 	"net/http"
 )
 
@@ -11,6 +13,22 @@ func NewRouter() *http.ServeMux {
 	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		status, err := fmt.Fprint(w, "Welcome to Rummi-Q-Server")
 		fmt.Println("Status:", status, "Error", err)
+	})
+
+	mux.HandleFunc("Post /games", func(w http.ResponseWriter, r *http.Request) {
+		gameLobby := lobby.NewLobby()
+		fmt.Println(gameLobby)
+
+		resp := map[string]any{
+			"game_id": gameLobby.ID,
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(resp)
+	})
+
+	mux.HandleFunc("/*", func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Not Found", http.StatusNotFound)
 	})
 
 	return mux

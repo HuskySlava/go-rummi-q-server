4b1ce79f5203b6db23361078ef29583269b07659
Slava Mihaelov <slava.m@pandats.com>
2025-12-26 18:02:12 +0200

    Router - Refactor, better structure

--- internal/transport/http/router.go
+++ internal/transport/http/router.go
@@ -5,83 +5,101 @@ import (
 	"fmt"
 	"github.com/google/uuid"
 	"go-rummi-q-server/internal/domain/lobbies"
+	"log"
 	"net/http"
 	"strings"
 )
 
+// ## Route actions ##
+
+func defaultRoute(w http.ResponseWriter, _ *http.Request) {
+	status, err := fmt.Fprint(w, "Welcome to Rummi-Q-Server")
+	log.Printf("Status: %d, Error: %v \n", status, err)
+}
+
+func createLobby(w http.ResponseWriter, _ *http.Request) {
+	gameLobby := lobbies.NewLobby()
+
+	resp := map[string]any{
+		"message": "Lobby Created",
+		"game_id": gameLobby.ID,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(resp)
+	if err != nil {
+		http.Error(w, "Failed to encode response: "+err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func joinLobby(w http.ResponseWriter, r *http.Request, lobbyID uuid.UUID) {
+	defer r.Body.Close()
+
+	var req struct {
+		PlayerName string `json:"player_name"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Invalid JSON: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if req.PlayerName == "" {
+		http.Error(w, "`player_name` is required", http.StatusBadRequest)
+		return
+	}
+
+	if !lobbies.LobbyExists(lobbyID) {
+		http.Error(w, "Lobby not found", http.StatusNotFound)
+		return
+	}
+
+	resp := map[string]string{
+		"message":     "Player joined game",
+		"player_name": req.PlayerName,
+		"lobby_id":    lobbyID.String(),
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	if err := json.NewEncoder(w).Encode(resp); err != nil {
+		http.Error(w, "Failed to encode response: "+err.Error(), http.StatusInternalServerError)
+	}
+}
+
+// ## Router handlers ##
+
 func NewRouter() *http.ServeMux {
 	mux := http.NewServeMux()
 
-	// Test route
-	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		status, err := fmt.Fprint(w, "Welcome to Rummi-Q-Server")
-		fmt.Println("Status:", status, "Error", err)
-	})
-
-	// Create new lobby
-	mux.HandleFunc("POST /lobbies", func(w http.ResponseWriter, r *http.Request) {
-		gameLobby := lobbies.NewLobby()
-
-		resp := map[string]any{
-			"message": "Lobby Created",
-			"game_id": gameLobby.ID,
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(resp)
-	})
-
-	// Join existing lobby
+	mux.HandleFunc("GET /", defaultRoute)
+	mux.HandleFunc("POST /lobbies", createLobby)
+	// Lobby actions
 	mux.HandleFunc("POST /lobbies/", func(w http.ResponseWriter, r *http.Request) {
 
-		path := r.URL.Path
-		parts := strings.Split(path, "/")
-		// Validate path structure "/games/{id}/join"
-		if len(parts) != 4 || parts[3] != "join" {
+		path := strings.Trim(r.URL.Path, "/")
+		urlParts := strings.Split(path, "/")
+
+		// Expecting: /lobbies/{id}/join
+		if len(urlParts) != 3 || urlParts[0] != "lobbies" {
 			http.NotFound(w, r)
 			return
 		}
-		lobbyId, err := uuid.Parse(parts[2])
+
+		lobbyID, err := uuid.Parse(urlParts[1])
 		if err != nil {
 			http.Error(w, "Invalid lobby ID", http.StatusBadRequest)
 			return
 		}
 
-		if r.Body == nil {
-			http.Error(w, "Request body required", http.StatusBadRequest)
-			return
+		action := urlParts[2]
+		switch action {
+		case "join":
+			joinLobby(w, r, lobbyID)
+		default:
+			http.NotFound(w, r)
 		}
 
-		var req struct {
-			PlayerName string `json:"player_name"`
-		}
-
-		err = json.NewDecoder(r.Body).Decode(&req)
-		if err != nil {
-			http.Error(w, "Invalid JSON: "+err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		if req.PlayerName == "" {
-			http.Error(w, "`player_name` is required", http.StatusBadRequest)
-			return
-		}
-
-		// Join logic
-		if !lobbies.LobbyExists(lobbyId) {
-			http.Error(w, "Lobby not found", http.StatusNotFound)
-			return
-		}
-
-		// Success
-		resp := map[string]string{
-			"message":     "Player joined game",
-			"player_name": req.PlayerName,
-			"lobby_id":    lobbyId.String(),
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(resp)
 	})
 
 	return mux

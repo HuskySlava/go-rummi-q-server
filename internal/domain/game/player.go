b74196b94ba98c2fc140a1b990f313e9e555f5d8
Slava Mihaelov <slava.m@pandats.com>
2026-01-05 21:26:45 +0200

    Player - Generate player ID function

--- internal/domain/game/player.go
+++ internal/domain/game/player.go
@@ -1,7 +1,11 @@
 package game
 
+import "crypto/rand"
+
+type PlayerID [8]byte
+
 type Player struct {
-	ID         int
+	ID         PlayerID
 	Name       string
 	Ready      bool
 	WinAmount  int
@@ -10,7 +14,7 @@ type Player struct {
 	Hand       []Meld
 }
 
-func NewPlayer(playerId int, playerName string) *Player {
+func NewPlayer(playerId PlayerID, playerName string) *Player {
 	return &Player{
 		ID:         playerId,
 		Name:       playerName,
@@ -19,4 +23,20 @@ func NewPlayer(playerId int, playerName string) *Player {
 		WinRate:    0,
 		Hand:       nil,
 	}
+}
+
+func GeneratePlayerId() (PlayerID, error) {
+	const charset = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	var id PlayerID
+
+	randomBytes := make([]byte, 8)
+	if _, err := rand.Read(randomBytes); err != nil {
+		return id, err
+	}
+
+	for i, b := range randomBytes {
+		id[i] = charset[b%64]
+	}
+
+	return id, nil
 }
